f0473114e9fdacd3d6d1565e0aa32fe70231d5e5
trying out tokens <ananiag2015@gmail.com>
2024-07-17 10:21:26 -0400

    july172

--- Python/practice/Practice06.py
+++ Python/practice/Practice06.py
@@ -1,4 +1,14 @@
 import json
 
-x = [1, 'simple', 'list']
-json.dump(x,f)
+# Python dictionary
+python_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "is_student": False,
+    "courses": ["Math", "Science"]
+}
+
+# Convert to JSON string
+json_string = json.dumps(python_dict)
+print(json_string)
